9f673cd883942906100e530e407a5a9796e37e16
Eugene Lukin <saumonminiball@gmail.com>
2024-04-01 00:29:47 +0300

    Build for Github Pages

---
+++ precache-manifest.b14bb168aa814c6b5a99a5b9b814ca88.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e0cddce6e99db78d22955f59548575",
+    "url": "/auction/index.html"
+  },
+  {
+    "revision": "c1e116c46740b1dac465",
+    "url": "/auction/static/css/main.a9bb995d.chunk.css"
+  },
+  {
+    "revision": "e4f58d7ad5b711e4c203",
+    "url": "/auction/static/js/2.92271e4c.chunk.js"
+  },
+  {
+    "revision": "e16ae08b468c1905db6c219edaefddf6",
+    "url": "/auction/static/js/2.92271e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e116c46740b1dac465",
+    "url": "/auction/static/js/main.dd7bff8d.chunk.js"
+  },
+  {
+    "revision": "62daf3993d201a7382b5ea3afc76ecda",
+    "url": "/auction/static/js/main.dd7bff8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93db5a54d7e837e4440a",
+    "url": "/auction/static/js/runtime-main.6630c2c9.js"
+  },
+  {
+    "revision": "89d32411d6d90efe602497c8d2d91696",
+    "url": "/auction/static/media/HelveticaNeue-Medium.89d32411.otf"
+  },
+  {
+    "revision": "a9a80ec3e8894233cca1758e811f619e",
+    "url": "/auction/static/media/HelveticaNeueCyr-Bold.a9a80ec3.woff"
+  },
+  {
+    "revision": "ea893f69b44ce11eeb0a42eb0e2113d4",
+    "url": "/auction/static/media/HelveticaNeueCyr-Light.ea893f69.woff"
+  },
+  {
+    "revision": "88b7a427cf1aec1984382459d5ca12e0",
+    "url": "/auction/static/media/HelveticaNeueCyr-Roman.88b7a427.woff"
+  },
+  {
+    "revision": "76bcf092145df51ecd0c0f971f039204",
+    "url": "/auction/static/media/HelveticaNeueCyr-Thin.76bcf092.woff"
+  },
+  {
+    "revision": "4bbe2c6ebabeeea3847561547606a1da",
+    "url": "/auction/static/media/auction.4bbe2c6e.svg"
+  },
+  {
+    "revision": "3b09595a3dfdeb355e5aa5ad9946543d",
+    "url": "/auction/static/media/dropout_proof.3b09595a.pdf"
+  },
+  {
+    "revision": "05b3159292dbb2a7486d96b528566444",
+    "url": "/auction/static/media/eblan.05b31592.png"
+  },
+  {
+    "revision": "4d69ad13fb27fe80f3007e120a908252",
+    "url": "/auction/static/media/pradenW.4d69ad13.png"
+  },
+  {
+    "revision": "8d1d23852b6ef5bf7af2df5506686801",
+    "url": "/auction/static/media/wheel.8d1d2385.svg"
+  }
+]);
